3be56a5197506515e0d1dea6150e482c82604cb3
joshlewski <lewandowski.joshua.d@gmail.com>
2021-06-29 21:11:31 -0400

    Started pokemonList array to store information of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Cheesesteak';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Beedrill',
+    height: 1,
+    types: ['bug','poison'],
+  },
+  {
+    name: 'Parasect',
+    height: 1,
+    types: ['grass','bug'],
+  },
+  {
+    name: 'Dodrio',
+    height: 1.8,
+    types: ['flying','normal'],
+  },
+];

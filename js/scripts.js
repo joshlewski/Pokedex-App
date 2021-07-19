9cfdb0eee90b3a009d2ce15d26f6d0932255fcd3
joshlewski <80796029+joshlewski@users.noreply.github.com>
2021-07-18 20:49:51 -0400

    Implemented DOM manipulation practices and added an event listener for click

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,72 @@
 let pokemonRepository = (function () {
 let pokemonList = [];
-return {
-    add: function(item) {
-      pokemonList.push(item);
-    },
-    getAll: function() {
-      return pokemonList;
-    }
-  };
+
+function add(pokemon) {
+   if (
+     typeof pokemon === "object" &&
+     "name" in pokemon &&
+     "height" in pokemon &&
+     "types" in pokemon
+   ) {
+     pokemonList.push(pokemon);
+   } else {
+     console.log("pokemon is not correct");
+   }
+ }
+
+ function getAll() {
+   return pokemonList;
+ }
+
+function showDetails(pokemon) {
+  console.log(pokemon);
+}
+
+ function addListItem(pokemon){
+   let pokemonList = document.querySelector(".pokemon-list");
+   let listpokemon = document.createElement("li");
+   let button = document.createElement("button");
+   button.innerText = pokemon.name;
+   button.classList.add("button-class");
+   listpokemon.appendChild(button);
+   pokemonList.appendChild(listpokemon);
+   button.addEventListener('click', function(event) {
+     showDetails(pokemon)
+   });
+ }
+
+ return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem
+ };
 })();
+
 pokemonRepository.add (
   {
     name: 'Beedrill',
     height: 1,
     types: ['bug','poison'],
   });
+
 pokemonRepository.add (
   {
     name: 'Parasect',
     height: 1,
     types: ['grass','bug'],
   });
+
 pokemonRepository.add (
   {
     name: 'Dodrio',
     height: 1.8,
     types: ['flying','normal'],
   });
+
 console.log(pokemonRepository.getAll());//puts the array into the console to be called
 // forEach to list Pokemon names and heights
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
 
-//Conditional to print " - Wow, that's big!" if height is over 1
-  if (pokemon.height > 1) {
-    document.write(" - Wow, that's big!");
-  }
-//Line break between each pokemon names
-  document.write('<br><br>')
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+
 });

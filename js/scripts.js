4991d268e8d597c07ef4b3d92eb2062f8358a44c
joshlewski <80796029+joshlewski@users.noreply.github.com>
2021-07-24 10:50:53 -0400

    Added an API for pokemon list to be derived from

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 let pokemonRepository = (function () {
 let pokemonList = [];
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 function add(pokemon) {
    if (
      typeof pokemon === "object" &&
-     "name" in pokemon &&
-     "height" in pokemon &&
-     "types" in pokemon
+     "name" in pokemon
    ) {
      pokemonList.push(pokemon);
    } else {
@@ -31,42 +30,60 @@ function showDetails(pokemon) {
    listpokemon.appendChild(button);
    pokemonList.appendChild(listpokemon);
    button.addEventListener('click', function(event) {
-     showDetails(pokemon)
+     showDetails(pokemon);
    });
  }
+
+ function loadList() {
+   return fetch(apiUrl).then(function (response) {
+     return response.json();
+   }).then(function (json) {
+     json.results.forEach(function (item) {
+       let pokemon = {
+         name: item.name,
+         detailsUrl: item.url
+       };
+       add(pokemon);
+     });
+   }).catch(function (e) {
+     console.error(e);
+   })
+ }
+
+ function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
 
  return {
    add: add,
    getAll: getAll,
-   addListItem: addListItem
+   addListItem: addListItem,
+   loadList: loadList,
+   loadDetails: loadDetails,
+   showDetails: showDetails
  };
 })();
 
-pokemonRepository.add (
-  {
-    name: 'Beedrill',
-    height: 1,
-    types: ['bug','poison'],
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
   });
-
-pokemonRepository.add (
-  {
-    name: 'Parasect',
-    height: 1,
-    types: ['grass','bug'],
-  });
-
-pokemonRepository.add (
-  {
-    name: 'Dodrio',
-    height: 1.8,
-    types: ['flying','normal'],
-  });
-
-console.log(pokemonRepository.getAll());//puts the array into the console to be called
-// forEach to list Pokemon names and heights
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
-
 });
